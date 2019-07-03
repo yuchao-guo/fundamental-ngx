49237abb9817997293a41fd8cc4ba2aa4158f198
O'Donnell <mikerodonnell89@users.noreply.github.com>
2019-07-03 17:32:01 -0600

    demonstrate broken datepicker/datetimepicker validity

--- docs/app/documentation/component-docs/date-picker/examples/date-picker-form-example.component.ts
+++ docs/app/documentation/component-docs/date-picker/examples/date-picker-form-example.component.ts
@@ -5,17 +5,19 @@ import { FormGroup, FormControl } from '@angular/forms';
     selector: 'fd-date-picker-form-example',
     template: `
         <form [formGroup]="customForm">
-            <fd-date-picker formControlName="date"></fd-date-picker>
+            <fd-date-picker [allowNull]="false" formControlName="date"></fd-date-picker>
         </form>
         
         Touched: {{customForm.controls.date.touched}}<br/>
         Dirty: {{customForm.controls.date.dirty}}<br/>
+        Form Valid: {{customForm.valid}}<br/>
+        FormControl Valid: {{customForm.controls.date.valid}}<br/>
 
         Selected Date: {{ customForm.controls.date.value.date ? customForm.controls.date.value.date.toDateString() : 'null' }}
     `
 })
 export class DatePickerFormExampleComponent {
     customForm = new FormGroup({
-        date: new FormControl({ date: new Date() })
+        date: new FormControl({ date: null })
     });
 };
