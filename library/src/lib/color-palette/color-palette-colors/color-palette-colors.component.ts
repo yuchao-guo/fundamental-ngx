82289c641cfcea5ef721a4daf7ba56d15b94cd3f
DorinR <dorin.rogov@gmail.com>
2019-08-16 15:45:04 -0400

    changed colors from color names to hex values

--- library/src/lib/color-palette/color-palette-colors/color-palette-colors.component.ts
+++ library/src/lib/color-palette/color-palette-colors/color-palette-colors.component.ts
@@ -7,9 +7,9 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class ColorPaletteColorsComponent {
 
-  colors: string[] = ['Gold', 'LightCoral', 'DeepPink', 'MediumVioletRed', 'MediumSlateBlue',
-    'CornflowerBlue', 'LightSeaGreen', 'OliveDrab', 'RoyalBlue', 'LightCyan', 'White', 'LightGray', 'DarkGray',
-    'DimGray', 'Black'];
+  colors: string[] = ['#ffd700', '#F08080', '#FF1493', '#c71585', '#7B68EE',
+    '#6495ed', '#20b2aa', '#6B8E23', '#4169E1', '#E0FFFF', '#FFFFFF', '#D3D3D3', '#A9A9A9',
+    '#696969', '#000000'];
 
   color: string;
 
