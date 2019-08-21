e61cd61042d249b7ad697296aa419a37742b1b20
DorinR <dorin.rogov@gmail.com>
2019-08-21 11:15:37 -0400

    removed color-palette testing example from the Alert docs page

--- docs/app/documentation/component-docs/alert/examples/color-palette-example.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'color-palette-example',
-    templateUrl: './color-palette-example.component.html'
-})
-export class ColorPaletteExampleComponent { }
