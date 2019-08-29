281bcd384b90a8762970dd4f08f282b4525da36c
DorinR <dorin.rogov@gmail.com>
2019-08-29 15:57:47 -0400

    changed where the color-palette components get its styles from in order to make SASS testing easier

--- library/src/lib/color-palette/color-palette.component.ts
+++ library/src/lib/color-palette/color-palette.component.ts
@@ -5,7 +5,10 @@ import { VariantConfiguration } from './models/variant-configuration';
 @Component({
     selector: 'fd-color-palette',
     templateUrl: './color-palette.component.html',
-    styleUrls: ['./color-palette.component.scss']
+    styleUrls: [
+        './color-palette.component.scss',
+        '../../../../node_modules/fundamental-styles/scss/components/color-palette.scss'
+    ]
 })
 export class ColorPaletteComponent implements OnInit {
     /** Used to determine which variant of the color-palette needs to be displayed */
@@ -16,9 +19,23 @@ export class ColorPaletteComponent implements OnInit {
     colorPaletteLauncherType: string;
 
     @Input()
-    defaultColors: string[] = ['#ffd700', '#F08080', '#FF1493', '#c71585', '#7B68EE',
-        '#6495ed', '#20b2aa', '#6B8E23', '#4169E1', '#E0FFFF', '#FFFFFF', '#D3D3D3', '#A9A9A9',
-        '#696969', '#000000'];
+    defaultColors: string[] = [
+        '#ffd700',
+        '#F08080',
+        '#FF1493',
+        '#c71585',
+        '#7B68EE',
+        '#6495ed',
+        '#20b2aa',
+        '#6B8E23',
+        '#4169E1',
+        '#E0FFFF',
+        '#FFFFFF',
+        '#D3D3D3',
+        '#A9A9A9',
+        '#696969',
+        '#000000'
+    ];
 
     /** Varible storing the color the user has clicked on */
     @Output()
@@ -32,7 +49,7 @@ export class ColorPaletteComponent implements OnInit {
     /** Variable that holds the color that is used if user selects 'default color' */
     defaultColor: string = 'Black';
 
-    constructor() { }
+    constructor() {}
 
     ngOnInit() {
         this.configureVariant(this.variant);
