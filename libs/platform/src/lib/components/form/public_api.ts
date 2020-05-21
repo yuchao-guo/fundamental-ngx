0158dbf2316e47c0949a574170513f92b8c7f25f
Deepak Kumar <deepak.kumar14@sap.com>
2020-05-21 22:40:22 +0530

    fix:public_api file updated

--- libs/platform/src/lib/components/form/public_api.ts
+++ libs/platform/src/lib/components/form/public_api.ts
@@ -6,10 +6,7 @@ export * from './input/fdp-input.module';
 export * from './input/input.component';
 export * from './combo-box/fdp-combo-box.module';
 export * from './combo-box/combo-box.component';
-<<<<<<< HEAD
 export * from './radio-group/radio-group.module';
 export * from './radio-group/radio-group.component';
-=======
 export * from './checkbox/checkbox.module';
 export * from './checkbox/checkbox.component';
->>>>>>> Rebased with Master
